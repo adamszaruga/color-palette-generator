fce2e5f7b572720e831ac38182cd12c057017585
Adam Szaruga <adamszaruga@gmail.com>
2019-02-15 13:03:36 -0500

    working

---
+++ Color.js
@@ -0,0 +1,12 @@
+const Color = props => {
+    
+    let { color, toggle } = props;
+    let { hex, isLocked } = color;
+
+    return (
+        <div style={{ backgroundColor: hex }} className="w-100 d-flex flex-column align-items-center justify-content-center">
+            <h1>{hex}</h1>
+            <button onClick={toggle} className={isLocked ? 'btn btn-dark' : 'btn btn-outline-dark'}>{isLocked ? 'UNLOCK' : 'LOCK'}</button>
+        </div>
+    )
+}
